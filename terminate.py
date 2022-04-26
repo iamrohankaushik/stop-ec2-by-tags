1bf602926475401212c5866d0a5e56d0e2bfc9a7
iamrohankaushik <100456918+iamrohankaushik@users.noreply.github.com>
2022-04-26 14:35:39 +0530

    Update terminate.py

--- terminate.py
+++ terminate.py
@@ -1,28 +1,18 @@
 import boto3
 import logging
 
-#setup simple logging for INFO
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-#define the connection
 ec2 = boto3.resource('ec2')
-# open connection to ec2
-#conn = get_ec2_conn()
-
-# get a list of all instances
-all_instances = [i for i in ec2.instances.all()]
-
 def lambda_handler(event, context):
 
-    #instances = ec2.instances.filter(Filters=filters)
-    # get instances with filter of running + with tag `Name`
-    instances = [i for i in ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}, {'Name':'tag:AutoOff', 'Values':['False']}])]
-
-    # make a list of filtered instances IDs `[i.id for i in instances]`
-    # Filter from all instances the instance that are not in the filtered list
-    instances_to_delete = [to_del for to_del in all_instances if to_del.id not in [i.id for i in instances]]
-
-    # run over your `instances_to_delete` list and terminate each one of them
-    for instance in instances_to_delete:
-        instance.stop()
+    filters = [{'Name': 'tag:AutoOff', 'Values': ['yes']}, {'Name': 'instance-state-name', 'Values': ['running']}]
+    instances = ec2.instances.filter(Filters=filters)
+    RunningInstances = [instance.id for instance in instances]
+    
+    if len(RunningInstances) > 0:
+        shuttingDown = ec2.instances.filter(InstanceIds=RunningInstances).terminate()
+        print("ShuttingDown")
+    else:
+        print("Nothing to see here")
