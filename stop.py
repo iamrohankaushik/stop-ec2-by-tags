90edfdfeb796ff639df273908a25b34dd53cf91f
iamrohankaushik <100456918+iamrohankaushik@users.noreply.github.com>
2022-04-26 14:13:38 +0530

    Create stop.py

---
+++ stop.py
@@ -0,0 +1,18 @@
+import boto3
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+ec2 = boto3.resource('ec2')
+def lambda_handler(event, context):
+
+filters = [{'Name': 'tag:AutoOff', 'Values': ['yes']},{'Name': 'instance-state-name', 'Values': ['running']}]
+instances = ec2.instances.filter(Filters=filters)
+RunningInstances = [instance.id for instance in instances]
+
+if len(RunningInstances) > 0:
+    shuttingDown = ec2.instances.filter(InstanceIds=RunningInstances).stop()
+    print ShuttingDown
+else:
+    print "Nothing to see here"
